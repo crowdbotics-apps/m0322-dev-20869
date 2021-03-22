cb0e69ce2e5911a054997ac93640e615ea725989
Crowdbotics <team@crowdbotics.com>
2021-03-22 15:13:36 +0000

    Update components

--- src/features/BlankScreen018049/screens/index.js
+++ src/features/BlankScreen018049/screens/index.js
@@ -1,3 +1,4 @@
+import { connect } from "react-redux"
 import React from "react"
 import {
   View,
@@ -16,27 +17,34 @@ import Icon from "react-native-vector-icons/FontAwesome"
 import Slider from "@react-native-community/slider"
 import { CheckBox } from "react-native-elements"
 import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
-import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import { widthPercentageToDP as wp } from "react-native-responsive-screen"
 
-export default class Blank extends React.Component {
+class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
   static navigationOptions = ({ navigation }) => {
-    return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />
-    }
+    return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
 
-  state = {}
-
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <Image
+        source={{
+          uri:
+            "https://crowdbotics-slack-dev.s3.amazonaws.com/media/resources/project/20869/6048c3f1-3868-46b3-a3da-6328d741a374.jpg"
+        }}
+      />
     </View>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16
-  }
-})
+const styles = StyleSheet.create({ View_1: {}, Image_3: {} })
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
